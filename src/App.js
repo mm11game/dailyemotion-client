c5bdf062f62caafdef626a28c46b5929fe9e59fa
kiunLee <mm11game@gmail.com>
2021-03-23 09:57:27 +0900

    d

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React, { useState, createContext, useMemo} from 'react';
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
-import EmailSignUp from './components/EmailSignUp';
-import LandingPage from './components/LandingPage';
-import List from './components/List';
-import EmailLogin from './components/EmailLogin';
-import MainPage from './components/MainPage';
-import Modified from './components/Modified';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer';
+import React, { useState, createContext, useMemo } from "react";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import EmailSignUp from "./components/EmailSignUp";
+import LandingPage from "./components/LandingPage";
+import List from "./components/List";
+import EmailLogin from "./components/EmailLogin";
+import MainPage from "./components/MainPage";
+import Modified from "./components/Modified";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer";
 //import axios from 'axios';
 
-import { initialState } from '../src/assets/state';
+import { initialState } from "../src/assets/state";
 
-
-export const AppContext = createContext()
+export const AppContext = createContext();
 
 function App() {
-
-const [items, setItems] = useState(initialState.items);
-const removeItem = (itemId) => {
-    setItems(items.filter(el => el.id !== itemId))
-  }
-const removeValue = useMemo(() => ({items, removeItem}), [removeItem])
+  const [items, setItems] = useState(initialState.items);
+  const removeItem = (itemId) => {
+    setItems(items.filter((el) => el.id !== itemId));
+  };
+  const removeValue = useMemo(() => ({ items, removeItem }), [removeItem]);
 
   return (
     <AppContext.Provider value={removeValue}>
@@ -29,17 +32,17 @@ const removeValue = useMemo(() => ({items, removeItem}), [removeItem])
         <Navbar />
         <Switch>
           <Route exact path="/signup" component={EmailSignUp} />
-          <Route path="/emaillogin" component={EmailLogin}  />
-          <Route path="/mainpage" component={MainPage} /> 
+          <Route path="/emaillogin" component={EmailLogin} />
+          <Route path="/mainpage" component={MainPage} />
           <Route path="/login" component={LandingPage} />
-          <Route path="/list" component={List}/>
+          <Route path="/list" component={List} />
           <Route path="/modified" component={Modified} />
           <Redirect path="*" to="/login" />
         </Switch>
         <Footer />
       </Router>
     </AppContext.Provider>
-  )
+  );
 }
 
 export default App;
